36f79412ad6af940be6306f2862f39d15fbf13f2
Wevertonbarbosa <wevertonlbs00@gmail.com>
2022-04-27 21:41:30 -0300

    Exercício aula 02 JS

---
+++ JS/Aula 02 - Console, variáveis e operadores JS/ExercícioSala.js
@@ -0,0 +1,11 @@
+const nome = 'Weverton';
+
+const peso = 75;
+
+const altura = 1.76;
+
+const imc = peso / (altura * altura);
+
+console.log(
+    `${nome} possui altura de ${altura} e peso ${peso} seu IMC é ${imc}`
+);
