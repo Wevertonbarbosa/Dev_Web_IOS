c8a54f5711d097c2c1aeeeab99aa444fbf7552f2
wevertonbarbosa <wevertonlbs00@gmail.com>
2022-05-14 01:05:33 -0300

    Exercício aula 03 Objetos, funções e eventos

--- JS/Aula JS EM SALA 03 - Objetos, Funções e Eventos/Main.js
+++ JS/Aula JS EM SALA 03 - Objetos, Funções e Eventos/Main.js
@@ -1,4 +1,31 @@
-const chamada = (alertCookie)=>{
+//Função simples com Alert
+function alertCookie() {
     alert('Você quer um cookie');
     console.log('Você quer um cookie');
-}
+};
+
+//Uma variavél com alert e Arrow Function
+const  alertSucesso = () => {
+    alert('Mensagem gerada com sucesso');
+    console.log('Mensagem gerada com sucesso');
+
+};
+
+//Um objeto com 5 propriedades e seus valores
+const notebook = [
+    {
+        marca: 'Dell',
+        memoria: '8G',
+        cor: 'Branco',
+        dono: 'Weverton', 
+        preco: 20.000
+    }
+];
+console.log(notebook);
+
+//Uma função simples com return convertendo anos para dias!!
+function diasanos (anos = 1, dias = 365) {
+    return dias * anos
+    
+};
+console.log(diasanos());
