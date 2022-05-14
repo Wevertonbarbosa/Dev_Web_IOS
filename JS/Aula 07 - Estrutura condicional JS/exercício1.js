945d1a92ceacd693ef7c02b499a2f02f7bd8b599
wevertonbarbosa <wevertonlbs00@gmail.com>
2022-05-14 04:00:30 -0300

    Aula 07 JS alterações

--- JS/Aula 07 - Estrutura condicional JS/exercício1.js
+++ JS/Aula 07 - Estrutura condicional JS/exercício1.js
@@ -41,15 +41,76 @@ let colaboradores = [
 }
 ]
 
-//Filtro do Vínculo
-//Operador ternário 
+//Filtro do Vínculo através do IF e Else
+if (colaboradores[0].vinculo == 'CLT', colaboradores[0].salario <= 1.220){
 
-if (colaboradores[0].vinculo == 'CLT'){
-    console.log(`É CLT, seu nome: ${colaboradores[0].nome}`);
+    console.log(`É CLT, seu nome: ${colaboradores[0].nome} e seu salário é ${colaboradores[0].salario}`);
 }
 else {
     console.log(`É PJ, seu nome: ${colaboradores[0].nome}`)
+} 
+
+if (colaboradores[1].vinculo == 'CLT'){
+    console.log(`É CLT, seu nome: ${colaboradores[1].nome}`);
 }
+else {
+    console.log(`É PJ, seu nome: ${colaboradores[1].nome}`)
+} 
+
+if (colaboradores[2].vinculo == 'CLT'){
+    console.log(`É CLT, seu nome: ${colaboradores[2].nome}`);
+}
+else {
+    console.log(`É PJ, seu nome: ${colaboradores[2].nome}`)
+} 
+
+if (colaboradores[3].vinculo == 'CLT'){
+    console.log(`É CLT, seu nome: ${colaboradores[3].nome}`);
+}
+else {
+    console.log(`É PJ, seu nome: ${colaboradores[3].nome}`)
+} 
+
+if (colaboradores[4].vinculo == 'CLT'){
+    console.log(`É CLT, seu nome: ${colaboradores[4].nome}`);
+}
+else {
+    console.log(`É PJ, seu nome: ${colaboradores[4].nome}`)
+} 
+
+
+
+if (colaboradores[0].salario <= 1.212){
+    console.log(`Este é o Desconto ${colaboradores[0].salario * (7.5 / 100)}`)
+}
+else {
+    console.log('')
+}
+
+if (colaboradores[0].salario >= 1.212){
+    console.log(`Este é o Desconto ${colaboradores[0].salario * (9.0 / 100)}`)
+}
+else {
+    console.log()
+}
+
+if (colaboradores[0].salario >= 2.427){
+    console.log(`Este é o Desconto ${colaboradores[0].salario * (9.0 / 100)}`)
+}
+else {
+    console.log()
+}   
+
+
+
+
+// else if (colaboradores[0].salario >= 2.427){
+//     console.log(`Este é o Desconto ${colaboradores[0].salario * (12.0 / 100)}`)
+// }
+// else if(colaboradores[0].salario >= 3.641){
+//     console.log(`Este é o Desconto ${colaboradores[0].salario * (14.0 / 100)}`)
+// }
+
 
 
 
