fc8e8e5b593ef79a666748e5ddda2dc3797bd81d
wevertonbarbosa <wevertonlbs00@gmail.com>
2022-05-17 21:53:41 -0300

    Aula 10 Classes e Funcoes

--- JS/Aula  09 - Métodos de alto nível para manipular arrays JS/Exercíciosala.js
+++ JS/Aula  09 - Métodos de alto nível para manipular arrays JS/Exercíciosala.js
@@ -1,5 +1,6 @@
 //Crie um array de objetos com quatros objetos e cada objeto tera 4 propriedades
-//retorne o nome de todos oa alunos que tirou nota maior que 6
+//Retorne o Nome de Todos os Alunos
+//retorne o nome de todos os alunos que tirou nota maior que 6
 //retorne o primeiro aluno que tirou nota 5
 //Utilize em todos os retornos com Arrow function
 
@@ -7,5 +8,22 @@ const escola = [
     {nome:'Roberto', idade: 20, curso: 'ADM', nota: 7 },
     {nome:'Luiz', idade: 22, curso: 'Ingles', nota: 10 },
     {nome:'Antonio', idade: 32, curso: 'Robótica', nota: 8 },
-    {nome:'Antonieta', idade: 30, curso: 'Culinária', nota: 6 }
+    {nome:'Antonieta', idade: 30, curso: 'Culinária', nota: 5 }
 ]
+
+//Método forEach para trazer todos os nomes dos alunos
+escola.forEach((TodosAlunos) => console.log(TodosAlunos.nome));
+
+
+//Método Filter no Console Log
+console.log(
+    escola.filter((nome)=>{
+    return nome.nota >= 6
+    })
+);
+//Método Find no Console Log
+console.log(
+    escola.find((primeiro)=>{ 
+    return primeiro.nota <= 5
+    })
+);
--- JS/Aula 07 - Estrutura condicional JS/exercício1.js
+++ JS/Aula 07 - Estrutura condicional JS/exercício1.js
@@ -41,6 +41,12 @@ let colaboradores = [
 }
 ]
 
+
+
+
+
+
+//
 let contratos = [
     colaboradores[0].vinculo == 'CLT' ? console.log (`É CLT, seu nome: ${colaboradores[0].nome}`) : console.log (`É PJ, seu nome: ${colaboradores[0].nome}`),
     colaboradores[1].vinculo == 'CLT' ? console.log (`É CLT, seu nome: ${colaboradores[1].nome}`) : console.log (`É PJ, seu nome: ${colaboradores[1].nome}`), 
@@ -50,12 +56,152 @@ let contratos = [
 ]
 
 
+//Calculo da Tabela INSS
+
+let aliquota = 0
+let inss    = 0
+//Primeiro funcionário
+if (colaboradores[0].vinculo == 'PJ'){
+  var desconto = 0
+}
+else if (colaboradores[0].salario > 1212 && colaboradores[0].salario <= 2427) {
+    colaboradores[0].salario * (7.5 / 100)
+    
+
+}
+else if (colaboradores[0].salario > 1212 && colaboradores[0].salario <= 2427){
+    colaboradores[0].salario * (9.0 / 100)
+}
+else if (colaboradores[0].salario > 2427 && colaboradores[0].salario <=  3641){
+    colaboradores[0].salario * (12.0 / 100)
+}
+else if (colaboradores[0].salario > 3641 && colaboradores[0].salario <= 7087){
+    colaboradores[0].salario * (12.0 / 100)
+}
+else if (colaboradores[0].salario > 3641 && colaboradores[0].salario <= 7087){
+    colaboradores[0].salario * (14.0 / 100)
+}
+else {
+    colaboradores[0].salario * (aliquota / 100)
+}
+console.log(`O vinculo do ${colaboradores[0].nome} é ${colaboradores[0].vinculo} e paga de INSS ${desconto}  `);
+// inss = colaboradores[0].salario * (aliquota /100)
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Segundo Funcionário
+let inss2 = 0
+if (colaboradores[1].vinculo == 'CLT')
+    if (colaboradores[1].salario <= 1212){
+        aliquota = 7.5
+    }
+    else if (colaboradores[1].salario > 1212 && colaboradores[1].salario <= 2427){
+        aliquota = 9.0
+    }
+    else if (colaboradores[1].salario > 2427 && colaboradores[1].salario <=  3641){
+        aliquota = 12.0
+    }
+    else if (colaboradores[1].salario > 3641 && colaboradores[1].salario <= 7087) {
+        aliquota = 14.0
+    }
+else {
+    aliquota = 0
+}
+
+inss2 = colaboradores[1].salario * (aliquota /100)
+
+
+
+//terceiro Funcionário
+let inss3 = 0
+if (colaboradores[2].vinculo == 'CLT')
+    if (colaboradores[2].salario <= 1212){
+        aliquota = 7.5
+    }
+    else if (colaboradores[2].salario > 1212 && colaboradores[2].salario <= 2427){
+        aliquota = 9.0
+    }
+    else if (colaboradores[2].salario > 2427 && colaboradores[2].salario <=  3641){
+        aliquota = 12.0
+    }
+    else if (colaboradores[2].salario > 3641 && colaboradores[2].salario <= 7087) {
+        aliquota = 14.0
+    }
+else {
+    aliquota = 0
+}
+
+inss3 = colaboradores[2].salario * (aliquota /100)
+
+
+
+//Quarto Funcionário
+let inss4 = 0
+if (colaboradores[3].vinculo == 'CLT')
+    if (colaboradores[3].salario <= 1212){
+        aliquota = 7.5
+    }
+    else if (colaboradores[3].salario > 1212 && colaboradores[3].salario <= 2427){
+        aliquota = 9.0
+    }
+    else if (colaboradores[3].salario > 2427 && colaboradores[3].salario <=  3641){
+        aliquota = 12.0
+    }
+    else if (colaboradores[3].salario > 3641 && colaboradores[3].salario <= 7087) {
+        aliquota = 14.0
+    }
+else {
+    aliquota = 0
+}
+
+inss4 = colaboradores[3].salario * (aliquota /100)
+
+
+//Quinto Funcionário
+let inss5 = 0
+if (colaboradores[4].vinculo == 'CLT')
+    if (colaboradores[4].salario <= 1212){
+        aliquota = 7.5
+
+    }
+    else if (colaboradores[4].salario > 1212 && colaboradores[4].salario <= 2427){
+        aliquota = 9.0
+        
+    }
+    else if (colaboradores[4].salario > 2427 && colaboradores[4].salario <=  3641){
+        aliquota = 12.0
+    
+    }
+    else if (colaboradores[4].salario > 3641 && colaboradores[4].salario <= 7087) {
+        aliquota = 14.0
+        
+    }
+else {
+    aliquota = 0
+}
 
 
 
