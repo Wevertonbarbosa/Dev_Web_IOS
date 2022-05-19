48367177e1ffdbc30d6618d47cce8b248eaefdb8
wevertonbarbosa <wevertonlbs00@gmail.com>
2022-05-19 18:39:37 -0300

    Aula 07 exercício1 JS

--- JS/Aula 07 - Estrutura condicional JS/exercício1.js
+++ JS/Aula 07 - Estrutura condicional JS/exercício1.js
@@ -1,3 +1,4 @@
+// Array com os colaboradores
 let colaboradores = [
 {
 
@@ -40,134 +41,140 @@ let colaboradores = [
 
 }
 ]
-
+// Array espelho (Não há nada dentro mas ajuda com método While)
+let inss = new Array(5);
 
 //Calculo da Tabela INSS
-let aliquota = 0
-let inss  = 0
+let aliquota = 0;
+
 
 //Primeiro Funcionário
-if (colaboradores[0].vinculo == 'CLT')
+if (colaboradores[0].vinculo == 'CLT'){
     if (colaboradores[0].salario <= 1212){
-        aliquota = 7.5
+        aliquota = 7.5;
     }
     else if (colaboradores[0].salario > 1212 && colaboradores[0].salario <= 2427){
-        aliquota = 9.0
+        aliquota = 9.0;
     }
     else if (colaboradores[0].salario > 2427 && colaboradores[0].salario <=  3641){
-        aliquota = 12.0
+        aliquota = 12.0;
     }
     else if (colaboradores[0].salario > 3641 && colaboradores[0].salario <= 7087) {
-         aliquota = 14.0
+         aliquota = 14.0;
     }
-else {
-    aliquota = 0
 }
-inss = colaboradores[0].salario * (aliquota /100)
+else {
+    aliquota = 0;
+}
+inss[0] = colaboradores[0].salario * (aliquota /100);
 
 
 //Segundo Funcionário
-let inss2 = 0
-if (colaboradores[1].vinculo == 'CLT')
+
+if (colaboradores[1].vinculo == 'CLT'){
     if (colaboradores[1].salario <= 1212){
-        aliquota = 7.5
+        aliquota = 7.5;
     }
     else if (colaboradores[1].salario > 1212 && colaboradores[1].salario <= 2427){
-        aliquota = 9.0
+        aliquota = 9.0;
     }
     else if (colaboradores[1].salario > 2427 && colaboradores[1].salario <=  3641){
-        aliquota = 12.0
+        aliquota = 12.0;
     }
     else if (colaboradores[1].salario > 3641 && colaboradores[1].salario <= 7087) {
-        aliquota = 14.0
+        aliquota = 14.0;
     }
+}
 else {
-    aliquota = 0
+    aliquota = 0;
 }
 
-inss2 = colaboradores[1].salario * (aliquota /100)
+inss[1] = colaboradores[1].salario * (aliquota /100);
 
 
 
 //terceiro Funcionário
-let inss3 = 0
-if (colaboradores[2].vinculo == 'CLT')
+
+if (colaboradores[2].vinculo == 'CLT'){
     if (colaboradores[2].salario <= 1212){
-        aliquota = 7.5
+        aliquota = 7.5;
     }
     else if (colaboradores[2].salario > 1212 && colaboradores[2].salario <= 2427){
-        aliquota = 9.0
+        aliquota = 9.0;
     }
     else if (colaboradores[2].salario > 2427 && colaboradores[2].salario <=  3641){
-        aliquota = 12.0
+        aliquota = 12.0;
     }
     else if (colaboradores[2].salario > 3641 && colaboradores[2].salario <= 7087) {
-        aliquota = 14.0
+        aliquota = 14.0;
     }
+}
 else {
-    aliquota = 0
+    aliquota = 0;
 }
 
-inss3 = colaboradores[2].salario * (aliquota /100)
+inss[2] = colaboradores[2].salario * (aliquota /100);
 
 
 
 //Quarto Funcionário
-let inss4 = 0
-if (colaboradores[3].vinculo == 'CLT')
+
+if (colaboradores[3].vinculo == 'CLT'){
     if (colaboradores[3].salario <= 1212){
-        aliquota = 7.5
+        aliquota = 7.5;
     }
     else if (colaboradores[3].salario > 1212 && colaboradores[3].salario <= 2427){
-        aliquota = 9.0
+        aliquota = 9.0;
     }
     else if (colaboradores[3].salario > 2427 && colaboradores[3].salario <=  3641){
-        aliquota = 12.0
+        aliquota = 12.0;
     }
     else if (colaboradores[3].salario > 3641 && colaboradores[3].salario <= 7087) {
-        aliquota = 14.0
+        aliquota = 14.0;
     }
+}
 else {
-    aliquota = 0
+    aliquota = 0;
 }
 
-inss4 = colaboradores[3].salario * (aliquota /100)
-
+inss[3] = colaboradores[3].salario * (aliquota /100);
+//console.log(inss4);
 
 //Quinto Funcionário
 
-let inss5 = 0
-if (colaboradores[4].vinculo == 'CLT')
+
+//aliquota = 0;
+if (colaboradores[4].vinculo == 'CLT'){
     if (colaboradores[4].salario <= 1212){
-        aliquota = 7.5
+        aliquota = 7.5;
 
     }
     else if (colaboradores[4].salario > 1212 && colaboradores[4].salario <= 2427){
-        aliquota = 9.0
+        aliquota = 9.0;
         
     }
     else if (colaboradores[4].salario > 2427 && colaboradores[4].salario <=  3641){
-        aliquota = 12.0
+        aliquota = 12.0;
     
     }
     else if (colaboradores[4].salario > 3641 && colaboradores[4].salario <= 7087) {
-        aliquota = 14.0
+        aliquota = 14.0;
         
     }
+}
 else {
-    aliquota = 0
+    aliquota = 0;
 }
 
-inss5 = colaboradores[4].salario * (aliquota /100)
-console.log(inss5);
+inss[4] = colaboradores[4].salario * (aliquota /100);
 
-// let contador2 = 0;
+let contador = 0;
 
-// while(contador2 <= 4){
-//     console.log(`Trabalhador ${colaboradores[contador2].nome}, sua contribuição no INSS:${inss}`)
-//     contador2++
+while(contador <= 4){
+    console.log(`Trabalhador ${colaboradores[contador].nome}, seu vinculo é : ${colaboradores[contador].vinculo}, sua contribuição no INSS: R$:${inss[contador]} `)
+    contador++
 
-// }
+}
 
 
 
--- JS/Aula 11 - Manipulando o DOM parte 1 - JS/Exercício.js
+++ JS/Aula 11 - Manipulando o DOM parte 1 - JS/Exercício.js
@@ -10,22 +10,23 @@ let titulo = document.createElement('H1');
 titulo.innerHTML = 'DOM! java Script';
 sessao.appendChild(titulo);
 titulo.style.borderBottom = 'solid 4px #000';
-console.log(document);
 
 
 let artigo = document.createElement('article');
 sessao.innerHTML 
 document.body.appendChild(artigo);
 
-let paragrafo = document.createElement('h1');
-paragrafo.innerHTML = 'Muito bom estudar';
+let titulo2 = document.createElement('h1');
+titulo2.innerHTML = 'Muito bom estudar!' 
+artigo.appendChild(titulo2);
+titulo2.style.fontSize = '20px'
+
+let paragrafo = document.createElement('P');
+paragrafo.innerHTML = 'Obrigado Professor Rafael';
 artigo.appendChild(paragrafo);
 titulo.style.borderBottom = 'solid 4px 	#000';
 titulo.style.fontSize = '15px'
 
-let titulo2 = document.createElement('P');
-titulo2.innerHTML = 'Obrigado Professor Rafael pela ajuda!' 
-artigo.appendChild(titulo2);
-titulo.style.borderBottom = 'solid 4px 	#000';
-titulo.style.fontSize = '15px' 
 
+
+console.log(document);
