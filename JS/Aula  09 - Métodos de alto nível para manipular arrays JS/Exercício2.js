bd5ef4120ad21aa77c3818edd253b5707a139244
wevertonbarbosa <wevertonlbs00@gmail.com>
2022-05-21 00:03:10 -0300

    Exercícios aula 09 manipular array JS

--- JS/Aula  09 - Métodos de alto nível para manipular arrays JS/Exercício2.js
+++ JS/Aula  09 - Métodos de alto nível para manipular arrays JS/Exercício2.js
@@ -5,22 +5,24 @@ const alunos = [
     {nome:'Marta', idade: 15  , nota:5, ano :'3°C'},
     {nome:'Brenno', idade: 19, nota:6, ano :'3°C'},
     {nome:'Maria', idade: 14, nota:4, ano :'1°F'}
-]
-alunos.forEach((nomes) => {  
-console.log(`${nomes.nome}`);
-})
+];
 
-// Filtro de Idades com Map 
-const anos = alunos.map((age)=>{
-    return age.idade
+console.log('Retorne somente o nome e o ano dos alunos:');
+alunos.forEach((nomes) => {  
+console.log(`Nome: ${nomes.nome}, Ano: ${nomes.ano}`);
 })
-console.log(anos);
 
 //função match O que eu definir ele irá buscar de uma forma mais enxuta
 //MATCH = retorna um array inde se a expressão regular usar o marcador global g ou não: Se o regexp usa o marcador g , o método match() retorna um array que armazena todos os resultados correspondentes. O resultado não contém os grupos de captura.
-console.log();
 let turma2 = alunos.filter((turma)=>{
 return turma.ano.match('2°')
 });
-
+console.log('Retorne apenas os alunos do segundo ano:');
 turma2.forEach((i)=> console.log(i.nome));
+
+//Método Reduce para calcular a média  
+// O número 0 no final serve para iniciar a soma do 0 com o primeiro número que esteja dentro do meu objeto
+let media = alunos.reduce ((recebe, atual)=> recebe + atual.nota / 6, 0);
+console.log('Média dos Alunos:');
+console.log(media.toPrecision(3));
+console.log('Os alunos não atingiram a meta requerida!');
