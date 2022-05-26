ea550b24a1ad25d643ad49064d36076e3f81a609
wevertonbarbosa <wevertonlbs00@gmail.com>
2022-05-25 21:49:45 -0300

    Aula 15 DOM parte 5 JS

--- JS/Aula 10 - Classes e Funções JS/Exercício1.js
+++ JS/Aula 10 - Classes e Funções JS/Exercício1.js
@@ -2,10 +2,21 @@ class Pessoa {
     constructor(nome, dataNascimento) {
     this.nome = nome;
     this.dataNascimento = dataNascimento;
-
     }
-
+    getIdade()
 }
+let pessoas = Array(10);
+pessoas[0] = new Pessoa ('Weverton', '2000 06 04');
+pessoas[1] = new Pessoa ('Luiz', '2002 07 02');
+pessoas[2] = new Pessoa, ('Pablo', '1999 05 18')
+pessoas[3] = new Pessoa ('Ronaldo', '2005 03 20')
+pessoas[4] = new Pessoa ('Bruno', '2000 08 15')
+pessoas[5] = new Pessoa ()
+pessoas[6] = new Pessoa ()
+pessoas[7] = new Pessoa ()
+pessoas[8] = new Pessoa ()
+pessoas[9] = new Pessoa ()
+
 
 // let data = new Date();
 // let data_completa = data.getHours().toFixed(2);
