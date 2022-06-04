765776a6bd10273563871abba35896cc391ec560
wevertonbarbosa <weevrtonlbs@gmail.com>
2022-06-04 18:02:19 -0300

    Exercício 14 JS

--- JS/Aula 14 - Manipulando o DOM Parte 4 JS/Exercício1.js
+++ JS/Aula 14 - Manipulando o DOM Parte 4 JS/Exercício1.js
@@ -1,33 +1,14 @@
 //trazendo todos as class e Id DO HTML
-const todo_formulario = document.querySelector('.meuform');
-const nome_form = document.querySelector('#name');
-const email_form = document.querySelector('#email');
-const user_email = document.querySelector('.resposta_email');
-const nascimento = document.querySelector('#dataNasc');
-const final = document.querySelector('#end_line');
-const mensagens = document.querySelector('.mensagem');
+let userName = document.getElementById('name');
+let userEmail = document.getElementById('email');
+let userDate = document.getElementById('dataNasc');
+let selecionar = document.getElementById('list');
+let botaoAdd = document.getElementById('btn1');
+let botaoExc = document.getElementById('btn2');
 
 // Método addEventListene Quando o usuário interagir
-todo_formulario.addEventListene('submit', clicando);
+botaoAdd.addEventListener('click', adicionar());
 
-function clicando(e) {
+function adicionar(e) {
     e.preventDefault();
-
-    if (nome_form.value === '' || email_form.value === '') {
-
-        mensagens.innerHTML = 'Por favor, preencha os dados.';
-        setTimeout(() => mensagens.remove(), 3000);
-    
-} else {
-    const li = document.createElement('li');
-    li.appendChild(
-    document.createTextNode(`${nome_form.value} : ${email_form.value} : ${nascimento.value}`)
-    );
-    userList.appendChild(li);
-    // Limpando o formulário
-    final.value = '';
-    email_form.value = '';
-    nascimento.getElementsByTagName('option')[0].selected = 'selected';
-    nome_form.focus();
 }
-};
