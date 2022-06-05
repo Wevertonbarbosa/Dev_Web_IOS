f2b6944b7d1e7339091a59a2832c29a6fa2747cc
wevertonbarbosa <wevertonlbs00@gmail.com>
2022-06-05 18:00:48 -0300

    alteracoes aula 14 JS

--- JS/Aula 14 - Manipulando o DOM Parte 4 JS/Exercício1.js
+++ JS/Aula 14 - Manipulando o DOM Parte 4 JS/Exercício1.js
@@ -3,73 +3,92 @@ let userName = document.getElementById('name');
 let userEmail = document.getElementById('email');
 let userDate = document.getElementById('dataNasc');
 let selecionar = document.getElementById('item');
-// let botaoAdd = document.getElementById('btn1');
 let conteudo = document.getElementById('formulario');
 let botaoExc = document.getElementById('btn2');
 let divMsg_name = document.getElementById('msg_invalido');
 let divMsg_email = document.getElementById('msg_erro');
 let divMsg_data = document.getElementById('msg_data')
+let my_form = document.getElementById('formulario');
 let dados = document.getElementById('informacao');
+let adicao = document.getElementById('btn1');
+
+//Vinculos em cada fução das mensagens.
+let verifIdade = false;
+let verifEmail = false;
+
+
 
 // Método addEventListene Quando o usuário interagir
-conteudo.addEventListener('click', adicionar);
+adicao.addEventListener('submit', adicionar);
+botaoExc.addEventListener('click', desfazer);
 
-
-
-
+//calculo da idade
 let dataAtual = new Date
-
-//Preciso inserir todas as funções que vou criar dentro do meu addEventListener(adicionar), ou criar outras para vincular as ações dentro do HTML
 
 
 
 //Criando a função
+
 function adicionar(e) {
     e.preventDefault();
+    validadeEmail();
+    validadeIdade();
 
-    let fullAno = new Date (userDate.value);
-    let idadeAtual = fullAno.getFullYear() - dataAtual.getFullYear();
+    if (userName.value === '' || userEmail === '') {
+        divMsg_name.innerHTML = 'Insira o nome'
 
-    if (userName.value === '' || userDate.value === '') {
-        
-
-        divMsg_name.innerHTML = 'Por favor, Insira seu nome completo.';
-        divMsg_data.innerHTML = 'Por favor, insira sua data de nascimento.';
-        divMsg_data.style.color = 'red';
-        divMsg_data.style.fontSize = '17px';
-        divMsg_name.style.color =' red';
-        divMsg_name.style.fontSize = '17px';
         setTimeout(() => divMsg_name.remove(), 3000);
-        setTimeout(() => divMsg_data.remove(), 3000);
         
-      
-    
-    } else if (idadeAtual < 18){
-
-        alert ('Precisa ter + 18 anos')
+    } else if (verifEmail === true && verifIdade === true) {
         
-    } else {
-
-        let info = document.createElement('li');
-        info.appendChild(document.createTextNode(` Seu Nome: ${userName.value} - Nascimento:${userDate.value}`));
-        dados.appendChild(info);
-
-
+        let letras = document.createElement('li');
+        letras.appendChild(document.createTextNode(` Candidato: ${userName.value}, E-mail: ${userEmail.value}, Nascimento: ${userDate.value}`));
+        dados.appendChild(letras);
+        userName.value = '';
+        userEmail.value = '';
+        //Dando Foco no nome do Usuário
+        userName.focus();
     }
 };
+
+// Validação da Idade 
+
+function validadeIdade() {
+    let idadeAtual = new Date(userDate);
+    let calculo = dataAtual.getFullYear() - idadeAtual.getFullYear();
+    if (dataAtual.getMonth() < idadeAtual.getMonth) calculo--; 
+    else if (dataAtual.getMonth() == idadeAtual.getMonth() && dataAtual.getDate() < idadeAtual.getDate()) calculo--; 
+        if (calculo < 18) {
+
+            alert('Precisa ter +18');
+            
+        }else {
+            verifIdade = true;
+        }
+    
+}
 
 
 
 
 //    validar email com RegExp
-userEmail.addEventListener('change', (e) => {
+function validadeEmail() {
+    
     let padrao = new RegExp(/.*@.*\..*/i);
     if (!padrao.test(userEmail.value)) {
-
-    // alert('Por favor, insira um e-mail válido.');
     divMsg_email.innerHTML = 'Por favor, insira um e-mail válido.';
     divMsg_email.style.color = 'red';
     divMsg_email.style.fontSize = '17px';
+    my_form.appendChild(divMsg_email);
     setTimeout(() => divMsg_email.remove(), 3000);
     }
-    });
+    };
+
+    //Função Desfazer botão de Excluir
+
+    function desfazer() {
+
+        let exc = prompt ('Insira a posição do candidato que deseja remover');
+        let todos = dados.getElementsByTagName('li');
+        dados.removeChild(todos[exc - 1])  
+    }
