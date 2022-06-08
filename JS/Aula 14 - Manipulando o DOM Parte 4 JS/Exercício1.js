fdabefa4bdbc1158989c05113ca83e49abc19f4f
wevertonbarbosa <wevertonlbs00@gmail.com>
2022-06-07 21:43:23 -0300

    aula 05 parte 2 React

--- JS/Aula 14 - Manipulando o DOM Parte 4 JS/Exercício1.js
+++ JS/Aula 14 - Manipulando o DOM Parte 4 JS/Exercício1.js
@@ -2,91 +2,93 @@
 let userName = document.getElementById('name');
 let userEmail = document.getElementById('email');
 let userDate = document.getElementById('dataNasc');
-let selecionar = document.getElementById('item');
-// let conteudo = document.getElementById('formulario');
+let selecionar = document.getElementById('Turma');
 let botaoExc = document.getElementById('btn2');
 let divMsg_name = document.getElementById('msg_invalido');
 let divMsg_email = document.getElementById('msg_erro');
-let divMsg_data = document.getElementById('msg_data')
+let divMsg_data = document.getElementById('msg_data');
 let my_form = document.getElementById('formulario');
 let dados = document.getElementById('informacao');
 let adicao = document.getElementById('btn1');
-
-//Vinculos em cada fução das mensagens.
-let verifIdade = false;
-let verifEmail = false;
-
+let divMsg_materia = document.getElementById('listMateria');
 
 
 // Método addEventListene Quando o usuário interagir
-my_form.addEventListener('submit', adicionar);
-botaoExc.addEventListener('click', desfazer);
+adicao.addEventListener('click', adicionar);
+// botaoExc.addEventListener('click', desfazer);
 
-//calculo da idade
-let dataAtual = new Date
-
-
-
-//Criando a função
-
+//Função para vincular todas as outras funções
 function adicionar(e) {
     e.preventDefault();
-    validadeEmail();
-    validadeIdade();
 
-    if (userName.value === '' || userEmail === '') {
-        divMsg_name.innerHTML = 'Insira o nome'
-
-        setTimeout(() => divMsg_name.remove(), 3000);
-        
-    } else if (verifEmail === true && verifIdade === true) {
-        
-        let letras = document.createElement('li');
-        letras.appendChild(document.createTextNode(` Candidato: ${userName.value}, E-mail: ${userEmail.value}, Nascimento: ${userDate.value}`));
-        dados.appendChild(letras);
-        userName.value = '';
-        userEmail.value = '';
-        //Dando Foco no nome do Usuário
-        userName.focus();
-    }
+    nome(); 
+    // idadeCerta();
+    emailVerif();
+  
 };
 
-// Validação da Idade 
+//Função nome
+function nome() {
 
-function validadeIdade() {
-    let idadeAtual = new Date(userDate);
-    let calculo = dataAtual.getFullYear() - idadeAtual.getFullYear();
-    if (dataAtual.getMonth() < idadeAtual.getMonth) calculo--; 
-    else if (dataAtual.getMonth() == idadeAtual.getMonth() && dataAtual.getDate() < idadeAtual.getDate()) calculo--; 
-        if (calculo < 18) {
+//calculo para saber se é de menor!
+let anoAtual = new Date();
+let dataAtual = new Date(userDate.value);
+let resultado = anoAtual.getFullYear() - dataAtual.getFullYear();
 
-            alert('Precisa ter +18');
-            
-        }else {
-            verifIdade = true;
-        }
+    if (userName.value == '') {
+
+        divMsg_name.innerHTML = 'Por Favor, colocar o nome.';
+        divMsg_name.style.fontSize = '16px';
+        divMsg_name.style.color = 'red';
+        divMsg_name.style.width = 'bold';
+        setTimeout(() => divMsg_name.remove(), 4000);
+
+    } else if (resultado < 18) {
+
+        alert ('So aceitamos candidatos +18.');
+        userName.value = '';
+        userEmail.value = '';
+        userDate.value = '';
+        selecionar.value = '';
+        
+
+    } else if (userDate.value == '') {
+       
+        divMsg_data.innerHTML = 'Por Favor, coloque sua data de nascimento.';
+        divMsg_data.style.fontSize = '16px';
+        divMsg_data.style.color = 'red';
+        divMsg_data.style.width = 'bold';
+        setTimeout(() => divMsg_data.remove(), 4000);
+
+    } else if (selecionar.value == vamos) {
+
+        divMsg_materia.innerHTML = 'Por favor, selecionar a matéria.'
+        divMsg_materia.style.fontSize = '16px';
+        divMsg_materia.style.color = 'red';
+        divMsg_materia.style.width = 'bold';
+        setTimeout(() => divMsg_materia.remove(), 4000);
+
+
+    }
     
-}
+};
 
 
-//    validar email com RegExp
-function validadeEmail() {
-    
+// let texto = document.createElement('li');
+// texto.appendChild(document.createTextNode(`Nome: ${userName.value} - Email: ${userEmail.value} - Nascimento: ${userDate.value} - Matéria ${selecionar.value}`));
+// dados.appendChild(texto);
+
+
+ 
+
+// Função para email
+function  emailVerif() {
     let padrao = new RegExp(/.*@.*\..*/i);
     if (!padrao.test(userEmail.value)) {
     divMsg_email.innerHTML = 'Por favor, insira um e-mail válido.';
+    divMsg_email.style.fontSize = '16px';
     divMsg_email.style.color = 'red';
-    divMsg_email.style.fontSize = '17px';
-    my_form.appendChild(divMsg_email);
+    divMsg_email.style.width = 'bold';
     setTimeout(() => divMsg_email.remove(), 3000);
-    }
     };
-
-    //Função Desfazer botão de Excluir
-
-    function desfazer() {
-
-        let exc = prompt ('Insira a posição do candidato que deseja remover');
-        let todos = dados.getElementsByTagName('li');
-        dados.removeChild(todos[exc - 1])  
-    }
+};
