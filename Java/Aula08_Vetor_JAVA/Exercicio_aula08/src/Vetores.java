0fb4d873836040c4996e1fe4d57aabfddf017c79
wevertonbarbosa <wevertonlbs00@gmail.com>
2022-07-05 21:07:44 -0300

    exercicio aula 08 JAVA

--- Java/Aula08_Vetor_JAVA/Exercicio_aula08/src/Vetores.java
+++ Java/Aula08_Vetor_JAVA/Exercicio_aula08/src/Vetores.java
@@ -1,15 +1,15 @@
+import java.util.Arrays;
 import java.util.Scanner;
+
 
 public class Vetores {
     public static void main(String[] args) {
 
         int vetorA[] = new int[4];
         int vetorB[] = new int[7];
-        int vetor[] = new int[11];
-
-
-
+        int vetorC[] = new int[vetorA.length + vetorB.length];
         Scanner info = new Scanner(System.in);
+
 
         for (int i = 0; i < vetorA.length; i++) {
             System.out.println("Digite um número #" + (i + 1));
@@ -24,10 +24,20 @@ public class Vetores {
 
         }
 
+        int count = 0;
 
-        for (int i = 0; i < vetor.length; i++) {
-            System.out.println("1°Lista" );
+        for (int indice : vetorA) {
+            vetorC[count] = indice;
+            count++;
+        }
 
+        for (int indice : vetorB) {
+            vetorC[count] = indice;
+            count++;
+        }
+
+        for (int c = 0; c < vetorC.length; c++) {
+            System.out.println(vetorC[c]);
         }
 
 
